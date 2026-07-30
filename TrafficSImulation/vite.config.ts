4a5dcd967826959615759ac2dfc727ca22b6a95f
KevinBarton1 <157058170+KevinBarton1@users.noreply.github.com>
2026-07-30 18:26:36 -0500

    Prompt 01 Complete

--- TrafficSImulation/vite.config.ts
+++ TrafficSImulation/vite.config.ts
@@ -4,10 +4,16 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: "dist",
     target: "es2018"
   },
   server: {
+    host: "127.0.0.1",
     port: 5173,
-    proxy: { "/api": "http://127.0.0.1:8000" }
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:8000"
+      }
+    }
   }
 });
